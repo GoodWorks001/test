848f11038522189615eecbd6545926589fb959a0
n.onoue <n.onoue@good-works.co.jp>
2018-11-26 15:55:36 +0900

    ハローワールドをふやしました

--- Sample.java
+++ Sample.java
@@ -2,6 +2,13 @@ public class Sample{
 	public static void main (String[] args){
 
 		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
 
 		}
 	}
